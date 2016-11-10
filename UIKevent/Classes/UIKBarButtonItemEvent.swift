32a3541c23b98075d8434f47931823cd27940f2e
Saulo Tauil <saulogt@gmail.com>
2016-11-09 18:38:36 -0800

    first commit

---
+++ UIKevent/Classes/UIKBarButtonItemEvent.swift
@@ -0,0 +1,45 @@
+//
+//  BarButtonItemEvent.swift
+//  personal_budget
+//
+//  Created by Saulo Tauil on 2016-11-08.
+//  Copyright © 2016 Saulo Tauil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+var BarItemClickClosureKey : Int16 = 0;
+
+extension UIBarButtonItem {
+    
+    typealias ClickClosure =  ((_ sender: UIBarButtonItem)->Void);
+
+    
+    class ClickEventWrapper {
+        var closure: ClickClosure?
+        init(closure: @escaping ClickClosure) {
+            self.closure = closure;
+        }
+    }
+    
+    func onClick( handler: @escaping ClickClosure){
+        
+        
+        objc_setAssociatedObject(self, &BarItemClickClosureKey, ClickEventWrapper(closure: handler) , .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        
+        target = self;
+        action = #selector(executeClick)
+        
+        
+    }
+    
+    func executeClick() -> Void {
+        if let wrapper = objc_getAssociatedObject(self, &BarItemClickClosureKey) as? ClickEventWrapper
+        {
+            wrapper.closure!(self);
+        }
+    }
+    
+}
---
+++ UIKevent/Classes/UIKDatePickerEvent.swift
@@ -0,0 +1,9 @@
+//
+//  UIKDatePickerEvent.swift
+//  Pods
+//
+//  Created by Saulo Tauil on 2016-11-09.
+//
+//
+
+import Foundation
